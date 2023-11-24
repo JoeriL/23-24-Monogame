8d34b55e8e4a4e5d02cc3c4c586d067472b770f4
JoeriL <joeri.lommers11@gmail.com>
2023-11-24 09:18:16 +0100

    iets

--- GameDev_TopDown/AnimationsCode/WindAnimation.cs
+++ GameDev_TopDown/AnimationsCode/WindAnimation.cs
@@ -20,25 +20,25 @@ namespace GameDev_TopDown.AnimationsCode
             switch (direction)
             {
                 case Direction.Up:
-                    for (int i = 48 * 30; i < 48 * 36 - 1; i += 48)
+                    for (int i = 48 * 30 + 8; i < 48 * 36 - 1; i += 48)
                     {
                         AddFrame(new AnimationFrame(new Rectangle(i, 1440, 48, 96)));
                     }
                     break;
                 case Direction.Down:
-                    for (int i = 48 * 42; i < 48 * 48 - 1; i += 48)
+                    for (int i = 48 * 42 + 8; i < 48 * 48 - 1; i += 48)
                     {
                         AddFrame(new AnimationFrame(new Rectangle(i, 1440, 48, 96)));
                     }
                     break;
                 case Direction.Left:
-                    for (int i = 48 * 36; i < 48 * 42 - 1; i += 48)
+                    for (int i = 48 * 36 + 8; i < 48 * 42 - 1; i += 48)
                     {
                         AddFrame(new AnimationFrame(new Rectangle(i, 1440, 48, 96)));
                     }
                     break;
                 case Direction.Right:
-                    for (int i = 48 * 24; i < 48 * 30 - 1; i += 48)
+                    for (int i = 48 * 24 + 8; i < 48 * 30 - 1; i += 48)
                     {
                         AddFrame(new AnimationFrame(new Rectangle(i, 1440, 48, 96)));
                     }
